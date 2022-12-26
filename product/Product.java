bba69ade7988fb3960ee7658a91f65f09ad2d33c
seoset <110677435+seoset@users.noreply.github.com>
2022-12-27 01:08:09 +0300

    Create Product.java

---
+++ product/Product.java
@@ -0,0 +1,34 @@
+package product;
+
+public class Product {
+    private final String name;
+    private final int weight;
+    private final int price;
+
+    public Product(String name, int weight, int price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                ", price=" + price +
+                '}';
+    }
+}
